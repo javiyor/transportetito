8834c6744a2a7d5ff08053236c5e60fca2eb354b
javiyor <155694389+javiyor@users.noreply.github.com>
2026-07-03 20:15:49 -0300

    fix: ruta resumen-arca usa array syntax [Controller::class, method]

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -298,6 +298,6 @@ Route::middleware([
 
         Route::get('/cierre', CierreCajaController::class)->name('cierre.index');
         Route::get('/cierre/print', CierreCajaPrintController::class)->name('cierre.print');
-        Route::get('/resumen-arca', ResumenArcaController::class)->name('resumen-arca');
+        Route::get('/resumen-arca', [ResumenArcaController::class, 'index'])->name('resumen-arca');
     });
 });
