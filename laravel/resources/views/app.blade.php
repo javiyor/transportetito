1fa2f0c11f59841edb9f24e27c17d5b4fe0f91b3
javiyor <155694389+javiyor@users.noreply.github.com>
2026-09-09 18:32:29 -0300

    fix(pwa): agrega meta mobile-web-app-capable no deprecado

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -11,6 +11,7 @@
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
         <link rel="manifest" href="/manifest.webmanifest">
         <meta name="theme-color" content="#4338ca">
+        <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
